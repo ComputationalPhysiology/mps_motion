0a2e03839d90032b5c2c0cfee4f39a50a8f7e62c
Henrik Finsberg <henriknf@simula.no>
2021-01-10 21:20:06 +0100

    Add a few standard methods

---
+++ mps_motion_tracking/farneback.py
@@ -0,0 +1,140 @@
+"""
+Farnebäck, G. (2003, June). Two-frame motion estimation based on polynomial expansion. In Scandinavian conference on Image analysis (pp. 363-370). Springer, Berlin, Heidelberg.
+
+
+"""
+
+import concurrent.futures
+from typing import Optional
+
+import cv2
+import numpy as np
+import tqdm
+
+from .utils import to_uint8
+
+
+def default_options():
+    return dict(
+        pyr_scale=0.5,
+        levels=3,
+        winsize=15,
+        iterations=3,
+        poly_n=5,
+        poly_sigma=1.2,
+        flags=0,
+    )
+
+
+def flow_map(args):
+    reference_image, image, *remaining_args = args
+
+    return flow(to_uint8(reference_image), to_uint8(image), *remaining_args)
+
+
+def flow(
+    image: np.ndarray,
+    reference_image: np.ndarray,
+    mask_flow: Optional[np.ndarray] = None,
+    pyr_scale: float = 0.5,
+    levels: int = 3,
+    winsize: int = 15,
+    iterations: int = 3,
+    poly_n: int = 5,
+    poly_sigma: float = 1.2,
+    flags: int = 0,
+):
+    return cv2.calcOpticalFlowFarneback(
+        reference_image,
+        image,
+        None,
+        pyr_scale,
+        levels,
+        winsize,
+        iterations,
+        poly_n,
+        poly_sigma,
+        flags,
+    )
+
+
+def get_velocities(
+    frames,
+    reference_image: np.ndarray,
+    mask_flow: Optional[np.ndarray] = None,
+    pyr_scale: float = 0.5,
+    levels: int = 3,
+    winsize: int = 15,
+    iterations: int = 3,
+    poly_n: int = 5,
+    poly_sigma: float = 1.2,
+    flags: int = 0,
+):
+
+    args = (
+        (
+            im,
+            ref,
+            mask_flow,
+            pyr_scale,
+            levels,
+            winsize,
+            iterations,
+            poly_n,
+            poly_sigma,
+            flags,
+        )
+        for (im, ref) in zip(np.rollaxis(frames, 2)[1:], np.rollaxis(frames, 2)[:-1])
+    )
+    num_frames = frames.shape[-1]
+    flows = np.zeros(
+        (reference_image.shape[0], reference_image.shape[1], 2, num_frames)
+    )
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for i, uv in tqdm.tqdm(
+            enumerate(executor.map(flow_map, args)), total=num_frames
+        ):
+            flows[:, :, :, i] = uv
+
+    return flows
+
+
+def get_displacements(
+    frames,
+    reference_image: np.ndarray,
+    mask_flow: Optional[np.ndarray] = None,
+    pyr_scale: float = 0.5,
+    levels: int = 3,
+    winsize: int = 15,
+    iterations: int = 3,
+    poly_n: int = 5,
+    poly_sigma: float = 1.2,
+    flags: int = 0,
+):
+
+    args = (
+        (
+            im,
+            reference_image,
+            mask_flow,
+            pyr_scale,
+            levels,
+            winsize,
+            iterations,
+            poly_n,
+            poly_sigma,
+            flags,
+        )
+        for im in np.rollaxis(frames, 2)
+    )
+    num_frames = frames.shape[-1]
+    flows = np.zeros(
+        (reference_image.shape[0], reference_image.shape[1], 2, num_frames)
+    )
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for i, uv in tqdm.tqdm(
+            enumerate(executor.map(flow_map, args)), total=num_frames
+        ):
+            flows[:, :, :, i] = uv
+
+    return flows
---
+++ mps_motion_tracking/utils.py
@@ -0,0 +1,68 @@
+import logging
+
+import cv2
+import numpy as np
+
+logger = logging.getLogger(__name__)
+
+try:
+    from numba import jit, prange
+except ImportError:
+    msg = (
+        "numba not found - Numba is just to speed up the motion tracking algorithm\n"
+        "To install numba use: pip install numba"
+    )
+    logger.warning(msg)
+    prange = range
+
+    # Create a dummy decorator
+    def jit(**params):
+        def decorator(f):
+            def wrap(*args, **kwargs):
+                return f(*args, **kwargs)
+
+            return wrap
+
+        return decorator
+
+
+STEP = 48
+QUIVER = (0, 0, 255)
+
+
+def draw_flow(img, flow, step=16):
+    h, w = img.shape[:2]
+    y, x = np.mgrid[step / 2 : h : step, step / 2 : w : step].reshape(2, -1).astype(int)
+    fx, fy = flow[y, x].T
+    lines = np.vstack([x, y, x + fx, y + fy]).T.reshape(-1, 2, 2)
+    lines = np.int32(lines + 0.5)
+    vis = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    cv2.polylines(vis, lines, 0, QUIVER, 5)
+    for (x1, y1), (_x2, _y2) in lines:
+        cv2.circle(vis, (x1, y1), 1, (0, 255, 0), -1)
+    return vis
+
+
+def draw_hsv(flow):
+    h, w = flow.shape[:2]
+    magnitude, angle = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+    hsv = np.zeros((h, w, 3), np.uint8)
+    # Sets image hue according to the optical flow
+    # direction
+    hsv[..., 0] = angle * 180 / np.pi / 2
+
+    # Sets image saturation to maximum
+    hsv[..., 1] = 255
+
+    # Sets image value according to the optical flow
+    # magnitude (normalized)
+    hsv[..., 2] = cv2.normalize(magnitude, None, 0, 255, cv2.NORM_MINMAX)
+
+    # Converts HSV to RGB (BGR) color representation
+    rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+
+    return rgb
+
+
+def to_uint8(img):
+    return (img / 256).astype(np.uint8)
