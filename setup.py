919084bac3f850e7d29eac2bfe14b1f46c5f687f
Henrik Finsberg <henriknf@simula.no>
2021-01-10 21:14:54 +0100

    Fix requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md") as readme_file:
 with open("HISTORY.md") as history_file:
     history = history_file.read()
 
-requirements = ["numpy", "python-opencv"]
+requirements = ["numpy", "opencv-python"]
 
 extras_require = {}
 
