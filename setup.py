9fd5eac58bc264c05929ac56bb252b0cb4746661
Henrik Finsberg <henriknf@simula.no>
2021-01-21 14:50:12 +0100

    Add dask dependency

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md") as readme_file:
 with open("HISTORY.md") as history_file:
     history = history_file.read()
 
-requirements = ["numpy", "opencv-python", "tqdm"]
+requirements = ["numpy", "opencv-python", "tqdm", "dask[array]"]
 
 extras_require = {
     "task-queue": ["redis", "rq", "SQLAlchemy"],
