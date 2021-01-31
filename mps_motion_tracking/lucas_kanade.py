e881fcb6d36a26a20412f967ea501d3082460ecc
Henrik Finsberg <henriknf@simula.no>
2021-01-31 22:57:19 +0100

    Add interpolation to lucas kanade

--- mps_motion_tracking/lucas_kanade.py
+++ mps_motion_tracking/lucas_kanade.py
@@ -5,14 +5,17 @@ Lucas, B. D., & Kanade, T. (1981). An iterative image registration technique wit
 http://cseweb.ucsd.edu/classes/sp02/cse252/lucaskanade81.pdf
 """
 import concurrent.futures
+import logging
 from collections import namedtuple
+from typing import Optional, Tuple
 
 import cv2
 import numpy as np
 import tqdm
 
-from .utils import to_uint8
+from . import utils
 
+logger = logging.getLogger(__name__)
 LKFlow = namedtuple("LKFlow", ["flow", "points"])
 
 
@@ -20,25 +23,60 @@ def default_options():
     return dict(
         winSize=(15, 15),
         maxLevel=2,
+        interpolate=False,
+        reshape=True,
         criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03),
+        step=16,
     )
+
+
+def rbfinterp2d_map(args):
+    return utils.rbfinterp2d(*args)
 
 
 def flow_map(args):
     reference_image, image, *remaining_args = args
 
-    return flow(to_uint8(reference_image), to_uint8(image), *remaining_args)
+    return _flow(
+        utils.to_uint8(reference_image), utils.to_uint8(image), *remaining_args
+    )
 
 
 def flow(
     image: np.ndarray,
     reference_image: np.ndarray,
-    points: np.ndarray,
-    winSize=(15, 15),
-    maxLevel=2,
+    points: Optional[np.ndarray] = None,
+    winSize: Tuple[int, int] = (15, 15),
+    maxLevel: int = 2,
     criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03),
-    return_points=False,
+    return_points: bool = False,
+    step: int = 16,
+    interpolate=True,
 ) -> np.ndarray:
+    if points is None:
+        points = get_uniform_reference_points(reference_image, step=step)
+    if image.dtype != np.uint8:
+        image = utils.to_uint8(image)
+    if reference_image.dtype != np.uint8:
+        reference_image = utils.to_uint8(reference_image)
+
+    f = _flow(image, reference_image, points, winSize, maxLevel, criteria)
+    if interpolate:
+        f = utils.rbfinterp2d(
+            points.squeeze(), f, np.arange(image.shape[1]), np.arange(image.shape[0])
+        )
+    return f
+
+
+def _flow(
+    image: np.ndarray,
+    reference_image: np.ndarray,
+    points: np.ndarray,
+    winSize: Tuple[int, int] = (15, 15),
+    maxLevel: int = 2,
+    criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03),
+) -> np.ndarray:
+
     next_points, status, error = cv2.calcOpticalFlowPyrLK(
         reference_image,
         image,
@@ -63,17 +101,21 @@ def get_uniform_reference_points(image, step=48):
 def get_displacements(
     frames,
     reference_image: np.ndarray,
-    step=48,
-    winSize=(15, 15),
-    maxLevel=2,
+    step: int = 16,
+    winSize: Tuple[int, int] = (15, 15),
+    maxLevel: int = 2,
     criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03),
-    return_refpoints=True,
+    return_refpoints: bool = False,
+    interpolate: bool = False,
+    reshape: bool = True,
+    resize: bool = True,
 ):
+    logger.info("Get displacements using Lucas Kanade")
 
     reference_points = get_uniform_reference_points(reference_image, step=step)
 
     args = (
-        (im, reference_image, reference_points, winSize, maxLevel, criteria, False)
+        (im, reference_image, reference_points, winSize, maxLevel, criteria)
         for im in np.rollaxis(frames, 2)
     )
     num_frames = frames.shape[-1]
@@ -81,9 +123,42 @@ def get_displacements(
 
     with concurrent.futures.ProcessPoolExecutor() as executor:
         for i, uv in tqdm.tqdm(
-            enumerate(executor.map(flow_map, args)), total=num_frames
+            enumerate(executor.map(flow_map, args)),
+            desc="Compute displacement",
+            total=num_frames,
         ):
             flows[:, :, i] = uv
+
+    if interpolate:
+        int_flows = np.zeros(
+            (reference_image.shape[0], reference_image.shape[1], 2, num_frames)
+        )
+        p = reference_points.squeeze()
+        x = np.arange(reference_image.shape[1])
+        y = np.arange(reference_image.shape[0])
+        int_args = ((p, f, x, y) for f in np.rollaxis(flows, 2))
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            for i, q in tqdm.tqdm(
+                enumerate(executor.map(rbfinterp2d_map, int_args)),
+                desc="Interpolate",
+                total=num_frames,
+            ):
+                int_flows[:, :, :, i] = q
+        flows = int_flows
+    else:
+        if reshape:
+            out = utils.reshape_lk(reference_points, flows)
+            flows = out
+        if resize:
+            new_shape = reference_image.shape[:2]
+            int_flows = np.zeros((new_shape[0], new_shape[1], 2, num_frames))
+            int_flows[:, :, 0, :] = utils.resize_frames(
+                flows[:, :, 0, :], new_shape=new_shape
+            )
+            int_flows[:, :, 1, :] = utils.resize_frames(
+                flows[:, :, 1, :], new_shape=new_shape
+            )
+            flows = int_flows
 
     if return_refpoints:
         return LKFlow(flows, reference_points)
