7b43f361b26daab65e86f2285cc3f25da31d3e7a
Henrik Finsberg <henriknf@simula.no>
2021-03-04 22:38:06 +0100

    Add helper classes to deal with multidimensional frame sequences

---
+++ mps_motion_tracking/frame_sequence.py
@@ -0,0 +1,127 @@
+from typing import Union
+
+import dask.array as da
+import numpy as np
+
+Array = Union[da.Array, np.ndarray]
+
+
+class FrameSequence:
+    """Object for holding a sequence of frames
+    For example a component of a Tensor / Vector
+    FrameSeqnecu
+
+    """
+
+    def __init__(self, array: Array):
+        """Constructor
+
+        Parameters
+        ----------
+        array : np.ndarray or dask array
+            Frame sequence of shape
+            (width, height, num_time_steps)
+
+        """
+        assert isinstance(array, (da.core.Array, np.ndarray))
+        self.array = array
+
+    @property
+    def shape(self):
+        return self.array.shape
+
+    def mean(self) -> Array:
+        return self.array.mean((0, 1))
+
+    def max(self) -> Array:
+        return self.array.max(2)
+
+    def __eq__(self, other) -> bool:
+        return np.isclose(self.array, other.array).all()
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.array.shape})"
+
+
+class VectorFrameSequence(FrameSequence):
+    """Object for holding a sequence of vectors.
+    For example displacement
+
+    """
+
+    def __init__(self, array: Array):
+        """Constructor
+
+        Parameters
+        ----------
+        array : np.ndarray or dask array
+            Tensor sequence of shape
+            (width, height, num_time_steps, 2)
+
+        """
+        super().__init__(array)
+        self._ns = np if isinstance(array, np.ndarray) else da
+        assert len(array.shape) == 4
+        assert array.shape[3] == 2
+
+    def norm(self) -> FrameSequence:
+        return FrameSequence(self._ns.linalg.norm(self.array, axis=3))
+
+    @property
+    def x(self) -> FrameSequence:
+        return FrameSequence(self.array[:, :, :, 0])
+
+    @property
+    def y(self) -> FrameSequence:
+        return FrameSequence(self.array[:, :, :, 1])
+
+
+class TensorFrameSequence(FrameSequence):
+    """Object for holding a sequence of tensors
+    For example Green-Lagrange strain tensor
+    and Cauchy stress tensor
+
+    """
+
+    def __init__(self, array: Array):
+        """Constructor
+
+        Parameters
+        ----------
+        array : np.ndarray or dask array
+            Tensor sequence of shape
+            (width, height, num_time_steps, 2, 2)
+
+        """
+        super().__init__(array)
+        self._ns = np if isinstance(array, np.ndarray) else da
+        assert len(array.shape) == 5
+        assert array.shape[3] == array.shape[4] == 2
+
+    def norm(self) -> FrameSequence:
+        return FrameSequence(self._ns.linalg.norm(self.array, axis=(3, 4)))
+
+    @property
+    def x(self) -> FrameSequence:
+        return FrameSequence(self.array[:, :, :, 0, 0])
+
+    @property
+    def y(self) -> FrameSequence:
+        return FrameSequence(self.array[:, :, :, 1, 1])
+
+    @property
+    def xy(self) -> FrameSequence:
+        return FrameSequence(self.array[:, :, :, 1, 0])
+
+    @property
+    def yx(self) -> FrameSequence:
+        return FrameSequence(self.array[:, :, :, 0, 1])
+
+    def compute_eigenvalues(self) -> VectorFrameSequence:
+        try:
+            # If we have a dask array
+            array = self.array.compute()
+        except AttributeError:
+            array = self.array
+
+        return VectorFrameSequence(np.linalg.eigvalsh(array))
---
+++ tests/test_frame_sequence.py
@@ -0,0 +1,111 @@
+import dask.array as da
+import numpy as np
+import pytest
+
+from mps_motion_tracking import frame_sequence as fs
+
+array_type = {da: da.core.Array, np: np.ndarray}
+
+
+@pytest.mark.parametrize("ns", [np, da])
+def test_frame_sequence(ns):
+    width = 10
+    height = 12
+    num_time_steps = 14
+
+    x = fs.FrameSequence(ns.random.random((width, height, num_time_steps)))
+
+    x_mean = x.mean()
+    assert isinstance(x_mean, array_type[ns])
+    assert x.mean().shape == (num_time_steps,)
+
+    x_max = x.max()
+    assert isinstance(x_max, array_type[ns])
+    assert x_max.shape == (width, height)
+
+
+@pytest.mark.parametrize("ns", [np, da])
+def test_vector_frame_sequence(ns):
+    width = 10
+    height = 12
+    num_time_steps = 14
+
+    arr = ns.random.random((width, height, num_time_steps, 2))
+
+    x = fs.VectorFrameSequence(arr)
+
+    x_mean = x.mean()
+    assert isinstance(x_mean, array_type[ns])
+    assert x.mean().shape == (num_time_steps, 2)
+
+    x_max = x.max()
+    assert isinstance(x_max, array_type[ns])
+    assert x_max.shape == (width, height, 2)
+
+    x_norm = x.norm()
+    assert isinstance(x_norm, fs.FrameSequence)
+    assert x_norm.shape == (width, height, num_time_steps)
+
+    x_x = x.x
+    assert isinstance(x_x, fs.FrameSequence)
+    assert x_x.shape == (width, height, num_time_steps)
+    assert x_x == fs.FrameSequence(arr[:, :, :, 0])
+
+    x_y = x.y
+    assert isinstance(x_y, fs.FrameSequence)
+    assert x_y.shape == (width, height, num_time_steps)
+    assert x_y == fs.FrameSequence(arr[:, :, :, 1])
+
+
+@pytest.mark.parametrize("ns", [np, da])
+def test_tensor_frame_sequence(ns):
+
+    width = 10
+    height = 12
+    num_time_steps = 14
+
+    arr = ns.ones((width, height, num_time_steps, 2, 2))
+
+    x = fs.TensorFrameSequence(arr)
+
+    x_x = x.x
+    assert isinstance(x_x, fs.FrameSequence)
+    assert x_x.shape == (width, height, num_time_steps)
+    assert x_x == fs.FrameSequence(arr[:, :, :, 0, 0])
+
+    x_y = x.y
+    assert isinstance(x_y, fs.FrameSequence)
+    assert x_y.shape == (width, height, num_time_steps)
+    assert x_y == fs.FrameSequence(arr[:, :, :, 1, 1])
+
+    x_xy = x.xy
+    assert isinstance(x_xy, fs.FrameSequence)
+    assert x_xy.shape == (width, height, num_time_steps)
+    assert x_xy == fs.FrameSequence(arr[:, :, :, 1, 0])
+
+    x_norm = x.norm()
+    assert isinstance(x_norm, fs.FrameSequence)
+    assert x_norm.shape == (width, height, num_time_steps)
+
+    x_mean = x.mean()
+    assert isinstance(x_mean, array_type[ns])
+    assert x.mean().shape == (num_time_steps, 2, 2)
+
+    x_max = x.max()
+    assert isinstance(x_max, array_type[ns])
+    assert x_max.shape == (width, height, 2, 2)
+
+    try:
+        # If we have a dask array
+        a = arr.compute()
+    except AttributeError:
+        a = arr
+
+    q = np.linalg.eigvalsh(a)
+    p = x.compute_eigenvalues()
+    assert p.shape == q.shape
+    assert np.isclose(p.array, q).all()
+
+
+if __name__ == "__main__":
+    test_vector_frame_sequence(np)
