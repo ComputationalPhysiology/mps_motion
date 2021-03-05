64cffb18680aa61de005a28bf9274e284b458704
Henrik Finsberg <henriknf@simula.no>
2021-03-05 09:53:32 +0100

    Use FrameSequence in Mechanics class

--- mps_motion_tracking/mechanics.py
+++ mps_motion_tracking/mechanics.py
@@ -1,31 +1,33 @@
 import logging
-import time
 from typing import Union
+
+try:
+    from functools import cached_property
+except ImportError:
+    # This is only supported in python 3.8 and above
+    try:
+        from cached_property import cached_property  # type: ignore
+    except ImportError as e:
+        raise ImportError(
+            "Please install cached_property - pip install cached_property"
+        ) from e
+
 
 import dask.array as da
 import numpy as np
+
+from . import frame_sequence as fs
 
 Array = Union[da.Array, np.ndarray]
 logger = logging.getLogger(__name__)
 
 
 def compute_gradients(displacement: Array, dx=1):
-    """
-    Computes gradients u_x, u_y, v_x, v_y from values in data
-
-    Args:
-        displacement - numpy array of dimensions T x X x Y x 2
-        dx - float; spatial difference between two points/blocks
-
-    Returns:
-        numpy array of dimensions T x X x Y x 2 x 2
-
-    """
     logger.info("Compute gradient")
-    dudx = 1 / dx * da.gradient(displacement[:, :, :, 0], axis=1)
-    dudy = 1 / dx * da.gradient(displacement[:, :, :, 0], axis=2)
-    dvdx = 1 / dx * da.gradient(displacement[:, :, :, 1], axis=1)
-    dvdy = 1 / dx * da.gradient(displacement[:, :, :, 1], axis=2)
+    dudx = 1 / dx * da.gradient(displacement[:, :, :, 0], axis=0)
+    dudy = 1 / dx * da.gradient(displacement[:, :, :, 0], axis=1)
+    dvdx = 1 / dx * da.gradient(displacement[:, :, :, 1], axis=0)
+    dvdy = 1 / dx * da.gradient(displacement[:, :, :, 1], axis=1)
 
     return da.moveaxis(
         da.moveaxis(da.stack([[dudx, dudy], [dvdx, dvdy]]), 0, -1), 0, -1
@@ -33,55 +35,12 @@ def compute_gradients(displacement: Array, dx=1):
 
 
 def compute_green_lagrange_strain_tensor(F: Array):
-    """
-    Computes the transpose along the third dimension of data; if data
-    represents the deformation gradient tensor F (over time and 2 spatial
-    dimensions) this corresponds to the Green-Lagrange strain tensor E.
-
-    Args:
-        numpy array of dimensions T x X x Y x 2 x 2
-
-    Returns
-        numpy array of dimensions T x X x Y x 2 x 2
-
-    """
     logger.info("Compute Green Lagrange strain")
     F_t = da.transpose(F, (0, 1, 2, 4, 3))
     C = da.matmul(F, F_t)
     E = 0.5 * (C - da.eye(2)[None, None, None, :, :])
 
     return E
-
-
-def princial_values(A: Array) -> np.ndarray:
-    """
-    Return the larges eigenvalue of a symmetric matrix.
-    I A is a strain tensor this is the same as the principal strain
-
-    Parameters
-    ----------
-    A : array
-        A numpy array or dask array consiting of the tensor
-
-    Returns
-    -------
-    np.ndarray
-        Eigenvalues
-    """
-    logger.info("Compute principal values")
-
-    if isinstance(A, da.Array):
-        t = time.perf_counter()
-        logger.info("Compute dask array")
-        A = A.compute()
-        logger.info(f"Computed dask array in {time.perf_counter() -t:.3f} seconds")
-
-    # E is symmetric so we can use this function which returns eigenvalues sored in acsending order
-    t = time.perf_counter()
-    eigenvalues = np.linalg.eigvalsh(A)
-    logger.info(f"Principal values computed in {time.perf_counter() - t:.3f} seconds")
-
-    return eigenvalues
 
 
 class Mechancis:
@@ -99,25 +58,7 @@ class Mechancis:
 
     def __init__(self, u: Array):
         logger.info("Convert displacement to dask array")
-        self.u = da.from_array(np.rollaxis(u, -1))
-
-    @property
-    def u_norm(self):
-        """Norm of displacement at all pixels and all time points"""
-        return da.linalg.norm(self.u, axis=-1)
-
-    @property
-    def u_mean_norm(self):
-        """Mean of norm of displacement at all pixels and all time points"""
-        return da.linalg.norm(self.u, axis=-1).mean((1, 2))
-
-    def u_mean(self, axis=None):
-        """Mean of displacement at all pixels and all time points at axis"""
-        return self.u[..., axis].mean((1, 2)).squeeze()
-
-    def u_max(self, axis=None):
-        """Max displacement over all time points at all pixels and all time points at axis"""
-        return self.u[..., axis].max(0).squeeze()
+        self.u = fs.VectorFrameSequence(da.from_array(np.swapaxes(u, 2, 3)))
 
     @property
     def width(self) -> int:
@@ -142,16 +83,22 @@ class Mechancis:
     def __repr__(self):
         return f"{self.__class__.__name__}(u = {self.u})"
 
-    def du(self, recompute=False) -> da.Array:
-        return compute_gradients(self.u)
+    @property
+    def du(self) -> fs.TensorFrameSequence:
+        return fs.TensorFrameSequence(compute_gradients(self.u.array))
 
-    def F(self, recompute=False) -> da.Array:
-        return self.du() + np.eye(2)[None, None, None, :, :]
+    @property
+    def F(self) -> fs.TensorFrameSequence:
+        return fs.TensorFrameSequence(self.du.array + da.eye(2)[None, None, None, :, :])
 
-    def E(self, recompute=False) -> da.Array:
-        return compute_green_lagrange_strain_tensor(self.F())
+    @property
+    def E(self) -> fs.TensorFrameSequence:
+        return fs.TensorFrameSequence(
+            compute_green_lagrange_strain_tensor(self.F.array)
+        )
 
-    def principal_strain(self, k=0, recompute=False) -> np.ndarray:
+    @cached_property
+    def principal_strain(self) -> fs.VectorFrameSequence:
         """Return the principal strain
 
         Parameters
@@ -167,8 +114,4 @@ class Mechancis:
         np.ndarray
             The kth principal strain
         """
-        assert k in (0, 1)
-        if not hasattr(self, "_principal_strain") or recompute:
-            E = self.E()
-            self._principal_strain = princial_values(E)
-        return self._principal_strain[..., -1 + k]
+        return self.E.compute_eigenvalues()
--- tests/test_mechanics.py
+++ tests/test_mechanics.py
@@ -2,28 +2,29 @@ import dask.array as da
 import numpy as np
 import pytest
 
-from mps_motion_tracking.mechanics import Mechancis
+from mps_motion_tracking import Mechancis
 
 
 def test_E_symmetry(mech_obj):
 
-    E = mech_obj.E()
+    E = mech_obj.E
     e12 = E[:, :, :, 0, 1]
     e21 = E[:, :, :, 1, 0]
     assert da.allclose(e12, e21).compute()
 
 
 def test_principal_strain(mech_obj):
-    e1 = mech_obj.principal_strain()
-    assert e1.shape == mech_obj.u.shape[:-1]
-    assert np.allclose(e1[0], 0)
+    e1 = mech_obj.principal_strain[:, :, :, 0]
+
+    assert np.allclose(e1[:, :, 0], 0)
 
     a1 = np.array([[0.005, 0.05], [0.05, 0.0]])
     eig = np.linalg.eigvals(a1)
-    assert np.allclose(e1[1], eig.max())
+    assert np.allclose(e1[:, :, 1], eig.min())
 
-    e2 = mech_obj.principal_strain(k=1)
-    assert np.allclose(e2[1], eig.min())
+    e2 = mech_obj.principal_strain[:, :, :, 1]
+
+    assert np.allclose(e2[:, :, 1], eig.max())
 
 
 @pytest.fixture
@@ -50,6 +51,23 @@ def mech_obj():
     return Mechancis(u)
 
 
+def test_shapes():
+
+    width = 10
+    height = 12
+    num_time_steps = 14
+
+    u = np.random.random((width, height, 2, num_time_steps))
+    m = Mechancis(u)
+
+    assert m.u.shape == (width, height, num_time_steps, 2)
+    assert m.du.shape == (width, height, num_time_steps, 2, 2)
+    assert m.F.shape == (width, height, num_time_steps, 2, 2)
+    assert m.E.shape == (width, height, num_time_steps, 2, 2)
+    assert m.principal_strain.shape == (width, height, num_time_steps, 2)
+
+
 if __name__ == "__main__":
     # main()
-    mech_obj()
+    # mech_obj()
+    test_shapes()
