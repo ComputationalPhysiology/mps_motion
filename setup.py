fdaf0e86e7b3d94874c8e44102f501db764eadcf
Henrik Finsberg <henriknf@simula.no>
2021-04-02 20:20:04 +0200

    Add typer as dependency

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md") as readme_file:
 with open("HISTORY.md") as history_file:
     history = history_file.read()
 
-requirements = ["numpy", "opencv-python", "tqdm", "dask[array]"]
+requirements = ["numpy", "opencv-python", "tqdm", "dask[array]", "typer"]
 
 extras_require = {
     "task-queue": ["redis", "rq", "SQLAlchemy", "Flask", "Flask-SQLAlchemy"],
