2f25c32b74abb1a67b6bd01e8a69df2f041ccf1a
Henrik Finsberg <henriknf@simula.no>
2021-08-19 11:15:45 +0200

    Some clean up in test for lucas kanade

--- tests/test_lucas_kanade.py
+++ tests/test_lucas_kanade.py
@@ -18,10 +18,7 @@ def test_flow_shape(size, step, interpolate, resize, expected_shape, expected_ty
     reference_image = 255 * np.random.randint(0, 255, size=size, dtype=np.uint8)
     image = 255 * np.random.randint(0, 255, size=size, dtype=np.uint8)
 
-    step = 16
-    h, w = reference_image.shape[:2]
-    grid = np.mgrid[step / 2 : w : step, step / 2 : h : step].astype(int)
-    reference_points = np.expand_dims(grid.astype(np.float32).reshape(2, -1).T, 1)
+    reference_points = lk.get_uniform_reference_points(reference_image, step)
     flow = lk.flow(
         image, reference_image, reference_points, interpolate=interpolate, resize=resize
     )
@@ -68,9 +65,7 @@ def test_flow_map():
     image = 255 * np.random.randint(0, 255, size=(64, 64), dtype=np.uint8)
 
     step = 16
-    h, w = reference_image.shape[:2]
-    grid = np.mgrid[step / 2 : w : step, step / 2 : h : step].astype(int)
-    reference_points = np.expand_dims(grid.astype(np.float32).reshape(2, -1).T, 1)
+    reference_points = lk.get_uniform_reference_points(reference_image, step)
     flow = lk.flow_map((image, reference_image, reference_points))
     assert flow.shape == (reference_points.shape[0], reference_points.shape[2])
     assert flow.dtype == np.float32
