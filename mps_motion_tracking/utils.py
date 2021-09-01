0c20a1b5fbb69dae8bfb248d26f5c5693366486d
Henrik Finsberg <henriknf@simula.no>
2021-09-01 08:32:41 +0200

    Refactor visualization module

--- mps_motion_tracking/utils.py
+++ mps_motion_tracking/utils.py
@@ -2,7 +2,6 @@ import logging
 import os
 from typing import Union
 
-import cv2
 import numpy as np
 
 logger = logging.getLogger(__name__)
@@ -84,67 +83,6 @@ class MPSData:
     @property
     def framerate(self):
         return int(1000 / np.mean(np.diff(self.time_stamps)))
-
-
-def _draw_flow(image, x, y, fx, fy):
-    QUIVER = (0, 0, 255)
-    lines = np.vstack([x, y, x + fx, y + fy]).T.reshape(-1, 2, 2)
-    lines = np.int32(lines + 0.5)
-    vis = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
-    cv2.polylines(vis, lines, 0, QUIVER, 5)
-    for (x1, y1), (_x2, _y2) in lines:
-        cv2.circle(vis, (x1, y1), 1, (0, 255, 0), -1)
-    return vis
-
-
-def draw_lk_flow(image, flow, reference_points):
-    x, y = reference_points.reshape(-1, 2).astype(int).T
-    fx, fy = flow.T
-    return _draw_flow(image, x, y, fx, fy)
-
-
-def draw_flow(image, flow, step=16):
-    """[summary]
-
-    Parameters
-    ----------
-    image : [type]
-        [description]
-    flow : [type]
-        [description]
-    step : int, optional
-        [description], by default 16
-
-    Returns
-    -------
-    [type]
-        [description]
-    """
-    h, w = image.shape[:2]
-    y, x = np.mgrid[step / 2 : h : step, step / 2 : w : step].reshape(2, -1).astype(int)
-    fx, fy = flow[y, x].T
-    return _draw_flow(image, x, y, fx, fy)
-
-
-def draw_hsv(flow):
-    h, w = flow.shape[:2]
-    magnitude, angle = cv2.cartToPolar(flow[..., 0], flow[..., 1])
-    hsv = np.zeros((h, w, 3), np.uint8)
-    # Sets image hue according to the optical flow
-    # direction
-    hsv[..., 0] = angle * 180 / np.pi / 2
-
-    # Sets image saturation to maximum
-    hsv[..., 1] = 255
-
-    # Sets image value according to the optical flow
-    # magnitude (normalized)
-    hsv[..., 2] = cv2.normalize(magnitude, None, 0, 255, cv2.NORM_MINMAX)
-
-    # Converts HSV to RGB (BGR) color representation
-    rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-
-    return rgb
 
 
 def to_uint8(img):
--- mps_motion_tracking/visu.py
+++ mps_motion_tracking/visu.py
@@ -7,8 +7,104 @@ import tqdm
 from . import utils
 
 
+def _draw_flow(image, x, y, fx, fy):
+    QUIVER = (0, 0, 255)
+    lines = np.vstack([x, y, x + fx, y + fy]).T.reshape(-1, 2, 2)
+    lines = np.int32(lines + 0.9)
+    vis = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+    cv2.polylines(vis, lines, 0, QUIVER, 1)
+    # for (x1, y1), (_x2, _y2) in lines:
+    #     cv2.circle(vis, (x1, y1), 1, (0, 255, 0), -1)
+    return vis
+
+
+def draw_lk_flow(image, flow, reference_points):
+    x, y = reference_points.reshape(-1, 2).astype(int).T
+    fx, fy = flow.T
+    return _draw_flow(image, x, y, fx, fy)
+
+
+def draw_flow(image, flow, step=16):
+    """[summary]
+
+    Parameters
+    ----------
+    image : [type]
+        [description]
+    flow : [type]
+        [description]
+    step : int, optional
+        [description], by default 16
+
+    Returns
+    -------
+    [type]
+        [description]
+    """
+    h, w = image.shape[:2]
+    y, x = np.mgrid[step / 2 : h : step, step / 2 : w : step].reshape(2, -1).astype(int)
+    fx, fy = flow[y, x].T
+    return _draw_flow(image, x, y, fx, fy)
+
+
+def draw_hsv(flow):
+    h, w = flow.shape[:2]
+    magnitude, angle = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+    hsv = np.zeros((h, w, 3), np.uint8)
+    # Sets image hue according to the optical flow
+    # direction
+    hsv[..., 0] = angle * 180 / np.pi / 2
+
+    # Sets image saturation to maximum
+    hsv[..., 1] = 255
+
+    # Sets image value according to the optical flow
+    # magnitude (normalized)
+    hsv[..., 2] = cv2.normalize(magnitude, None, 0, 255, cv2.NORM_MINMAX)
+
+    # Converts HSV to RGB (BGR) color representation
+    rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+
+    return rgb
+
+
 def quiver_video(
-    data: utils.MPSData, vectors: np.ndarray, path: utils.PathLike
+    data: utils.MPSData, vectors: np.ndarray, path: utils.PathLike, convert: bool = True
+) -> None:
+
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+
+    width = data.size_y
+    height = data.size_x
+    fps = data.framerate
+
+    p = Path(path).with_suffix(".mp4")
+    out = cv2.VideoWriter(p.as_posix(), fourcc, fps, (width, height))
+
+    time_axis = vectors.shape.index(data.num_frames)
+    for i in tqdm.tqdm(range(data.num_frames), desc=f"Create quiver video at {p}"):
+        im = utils.to_uint8(data.frames[:, :, i])
+        flow = np.take(vectors, i, axis=time_axis)
+        out.write(draw_flow(im, flow))
+
+    out.release()
+    cv2.destroyAllWindows()
+
+    if convert:
+        import imageio
+
+        tmp_path = p.parent.joinpath(p.stem + "_tmp").with_suffix(".mp4")
+        p.rename(tmp_path)
+        video = imageio.read(tmp_path)
+        imageio.mimwrite(p, (np.swapaxes(d, 0, 1) for d in video.iter_data()), fps=fps)
+        tmp_path.unlink()
+
+
+def hsv_video(
+    data: utils.MPSData,
+    vectors: np.ndarray,
+    path: utils.PathLike,
+    convert: bool = False,
 ) -> None:
 
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
@@ -20,31 +116,18 @@ def quiver_video(
     p = Path(path).with_suffix(".mp4")
 
     out = cv2.VideoWriter(p.as_posix(), fourcc, fps, (width, height))
-
-    for i in tqdm.tqdm(range(data.num_frames), desc=f"Create quiver video at {p}"):
-        im = utils.to_uint8(data.frames[:, :, i])
-        flow = vectors[:, :, :, i]
-        out.write(utils.draw_flow(im, flow))
-
-    out.release()
-    cv2.destroyAllWindows()
-
-
-def hsv_video(data: utils.MPSData, vectors: np.ndarray, path: utils.PathLike) -> None:
-
-    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-
-    width = data.size_y
-    height = data.size_x
-    fps = data.framerate
-
-    p = Path(path).with_suffix(".mp4")
-
-    out = cv2.VideoWriter(p.as_posix(), fourcc, fps, (width, height))
-
+    time_axis = vectors.shape.index(data.num_frames)
     for i in tqdm.tqdm(range(data.num_frames), desc=f"Create HSV movie at {p}"):
-        flow = vectors[:, :, :, i]
-        out.write(utils.draw_hsv(flow))
+        flow = np.take(vectors, i, axis=time_axis)
+        out.write(draw_hsv(flow))
 
     out.release()
     cv2.destroyAllWindows()
+
+    if convert:
+        import imageio
+
+        tmp_path = p.parent.joinpath(p.stem + "_tmp").with_suffix(".mp4")
+        p.rename(tmp_path)
+        video = imageio.read(tmp_path)
+        imageio.mimwrite(p, video.iter_data(), fps=fps)
