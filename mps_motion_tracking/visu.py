2f9652fecf37e3baa634e6405165472aaf2ec326
Henrik Nicolay Finsberg <henriknf@simula.no>
2021-09-01 12:36:47 +0200

    Make it possible to pass step and scale to quiver visualization function

--- mps_motion_tracking/visu.py
+++ mps_motion_tracking/visu.py
@@ -10,11 +10,11 @@ from . import utils
 def _draw_flow(image, x, y, fx, fy):
     QUIVER = (0, 0, 255)
     lines = np.vstack([x, y, x + fx, y + fy]).T.reshape(-1, 2, 2)
-    lines = np.int32(lines + 0.9)
+    lines = np.int32(lines + 0.5)
     vis = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
-    cv2.polylines(vis, lines, 0, QUIVER, 1)
-    # for (x1, y1), (_x2, _y2) in lines:
-    #     cv2.circle(vis, (x1, y1), 1, (0, 255, 0), -1)
+    cv2.polylines(vis, lines, 0, QUIVER, thickness=5, lineType=8)
+    for (x1, y1), (_x2, _y2) in lines:
+        cv2.circle(vis, (x1, y1), 1, (0, 255, 0), -1)
     return vis
 
 
@@ -24,7 +24,7 @@ def draw_lk_flow(image, flow, reference_points):
     return _draw_flow(image, x, y, fx, fy)
 
 
-def draw_flow(image, flow, step=16):
+def draw_flow(image, flow, step=16, scale: float = 1.0):
     """[summary]
 
     Parameters
@@ -44,7 +44,7 @@ def draw_flow(image, flow, step=16):
     h, w = image.shape[:2]
     y, x = np.mgrid[step / 2 : h : step, step / 2 : w : step].reshape(2, -1).astype(int)
     fx, fy = flow[y, x].T
-    return _draw_flow(image, x, y, fx, fy)
+    return _draw_flow(image, x, y, scale * fx, scale * fy)
 
 
 def draw_hsv(flow):
@@ -69,7 +69,12 @@ def draw_hsv(flow):
 
 
 def quiver_video(
-    data: utils.MPSData, vectors: np.ndarray, path: utils.PathLike, convert: bool = True
+    data: utils.MPSData,
+    vectors: np.ndarray,
+    path: utils.PathLike,
+    step: int = 16,
+    scale: float = 1.0,
+    convert: bool = True,
 ) -> None:
 
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
@@ -79,13 +84,15 @@ def quiver_video(
     fps = data.framerate
 
     p = Path(path).with_suffix(".mp4")
+    if p.is_file():
+        p.unlink()
     out = cv2.VideoWriter(p.as_posix(), fourcc, fps, (width, height))
 
     time_axis = vectors.shape.index(data.num_frames)
     for i in tqdm.tqdm(range(data.num_frames), desc=f"Create quiver video at {p}"):
         im = utils.to_uint8(data.frames[:, :, i])
         flow = np.take(vectors, i, axis=time_axis)
-        out.write(draw_flow(im, flow))
+        out.write(draw_flow(im, flow, step=step, scale=scale))
 
     out.release()
     cv2.destroyAllWindows()
