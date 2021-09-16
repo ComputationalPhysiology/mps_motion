Fix scaling

mps_motion_tracking/motion_tracking.py
@@ -146,23 +146,25 @@ class OpticalFlow:
 
         if scale > 1.0:
             raise ValueError("Cannot have scale larger than 1.0")
+
+        scaled_data = data
         if scale < 1.0:
-            data = scaling.resize_data(data, scale)
+            scaled_data = scaling.resize_data(data, scale)
             _, reference_image, _ = get_referenece_image(
-                self.reference_frame, data.frames, data.time_stamps
+                self.reference_frame, scaled_data.frames, scaled_data.time_stamps
             )
 
         if not hasattr(self, "_displacement") or recompute:
 
-            u = self._get_displacements(data.frames, reference_image, **self.options)
+            u = self._get_displacements(
+                scaled_data.frames, reference_image, **self.options
+            )
 
             dx = 1
             u /= scale
-
             if unit == "um":
-                # um_per_pixel is allready divided be scale
-                u *= data.info.get("um_per_pixel", 1.0) * scale
-                dx *= data.info.get("um_per_pixel", 1.0) * scale
+                u *= scaled_data.info.get("um_per_pixel", 1.0)
+                dx *= scaled_data.info.get("um_per_pixel", 1.0)
 
             U = da.from_array(np.swapaxes(u, 2, 3))
             self._displacement = fs.VectorFrameSequence(U, dx=dx, scale=scale)

mps_motion_tracking/visu.py
@@ -4,7 +4,7 @@ import cv2
 import numpy as np
 import tqdm
 
-from . import utils
+from . import scaling, utils
 
 
 def _draw_flow(image, x, y, fx, fy):
@@ -73,28 +73,52 @@ def quiver_video(
     vectors: np.ndarray,
     path: utils.PathLike,
     step: int = 16,
-    scale: float = 1.0,
+    vector_scale: float = 1.0,
+    frame_scale: float = 1.0,
     convert: bool = True,
     velocity: bool = False,
 ) -> None:
 
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
 
+    if frame_scale < 1.0:
+        data = scaling.resize_data(data, frame_scale)
+
     width = data.size_y
     height = data.size_x
     fps = data.framerate
+
+    num_frames = data.num_frames - 1 if velocity else data.num_frames
+
+    # Check shapes
+    try:
+        time_axis = vectors.shape.index(num_frames)
+    except ValueError as ex:
+        msg = (
+            "Time axis for frames and vetors does not match. "
+            f"Got frames of shape {data.frames.shape}, and "
+            f"vector of shape {vectors.shape}."
+        )
+        raise ValueError(msg) from ex
+
+    vector_shape = (vectors.shape[0], vectors.shape[1], vectors.shape[3])
+    if not data.frames.shape == vector_shape:
+        msg = (
+            "Shape of frames and vector does not match. "
+            f"Got frames of shape {data.frames.shape}, and "
+            f"vector of shape {vectors.shape}."
+            f"Extected frames to have shape {vector_shape}."
+        )
+        raise ValueError(msg)
 
     p = Path(path).with_suffix(".mp4")
     if p.is_file():
         p.unlink()
     out = cv2.VideoWriter(p.as_posix(), fourcc, fps, (width, height))
-
-    num_frames = data.num_frames - 1 if velocity else data.num_frames
-    time_axis = vectors.shape.index(num_frames)
     for i in tqdm.tqdm(range(num_frames), desc=f"Create quiver video at {p}"):
         im = utils.to_uint8(data.frames[:, :, i])
         flow = np.take(vectors, i, axis=time_axis)
-        out.write(draw_flow(im, flow, step=step, scale=scale))
+        out.write(draw_flow(im, flow, step=step, scale=vector_scale))
 
     out.release()
     cv2.destroyAllWindows()

tests/test_scaling.py
@@ -4,7 +4,15 @@ from mps_motion_tracking import Mechancis, OpticalFlow, scaling, utils
 
 
 def test_resize_data():
-    pass
+    frames = np.ones((100, 100, 3))
+    times = np.linspace(0, 5000, frames.shape[-1])
+    info = dict(um_per_pixel=1.0)
+    data = utils.MPSData(frames=frames, time_stamps=times, info=info)
+    scale = 0.5
+    scaled_data = scaling.resize_data(data, scale)
+    assert scaled_data.info["um_per_pixel"] == info["um_per_pixel"] / scale
+    assert scaled_data.frames.shape == (50, 50, 3)
+    assert np.isclose(scaled_data.time_stamps, times).all()
 
 
 def test_resize_frames():