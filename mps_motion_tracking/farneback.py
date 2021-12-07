35a5c38c5e9dbfdacc2c68e72fe6285dceb17324
Henrik Finsberg <henriknf@simula.no>
2021-12-07 12:50:21 +0100

    Numpy.stack + dask.from_array instead of dask.stack - it seems to run faster?

--- mps_motion_tracking/farneback.py
+++ mps_motion_tracking/farneback.py
@@ -191,10 +191,90 @@ def get_displacements(
         )
 
     with ProgressBar():
+        flows = np.stack(*da.compute(all_flows), axis=2)
+
+    logger.info("Done running Farneback's algorithm")
+    return da.from_array(flows)
+
+
+def get_velocities(
+    frames,
+    pyr_scale: float = 0.5,
+    levels: int = 3,
+    winsize: int = 15,
+    iterations: int = 3,
+    poly_n: int = 5,
+    poly_sigma: float = 1.2,
+    flags: int = 0,
+) -> utils.Array:
+    """Compute the optical flow using the Farneback method from
+    the reference frame to all other frames
+
+    Parameters
+    ----------
+    frames : np.ndarray
+        The frames with some moving objects
+    pyr_scale : float, optional
+        parameter, specifying the image scale (<1) to build pyramids
+        for each image; pyr_scale=0.5 means a classical pyramid,
+        where each next layer is twice smaller than the previous
+        one, by default 0.5
+    levels : int, optional
+        number of pyramid layers including the initial image; levels=1
+        means that no extra layers are created and only the original
+        images are used, by default 3
+    winsize : int, optional
+        averaging window size; larger values increase the algorithm
+        robustness to image noise and give more chances for fast motion
+        detection, but yield more blurred motion field, by default 15
+    iterations : int, optional
+        number of iterations the algorithm does at each pyramid level, by default 3
+    poly_n : int, optional
+        size of the pixel neighborhood used to find polynomial expansion in each pixel.
+        larger values mean that the image will be approximated with smoother surfaces,
+        yielding more robust algorithm and more blurred motion field,
+        typically poly_n =5 or 7., by default 5
+    poly_sigma : float, optional
+        standard deviation of the Gaussian that is used to smooth derivatives used as a
+        basis for the polynomial expansion; for poly_n=5, you can set poly_sigma=1.1,
+        for poly_n=7, a good value would be poly_sigma=1.5, by default 1.2
+    flags : int, optional
+         By default 0. operation flags that can be a combination of the following:
+         - OPTFLOW_USE_INITIAL_FLOW uses the input flow as an initial flow approximation.
+         - OPTFLOW_FARNEBACK_GAUSSIAN uses the Gaussian winsize x winsize filter
+            instead of a box filter of the same size for optical flow estimation;
+            usually, this option gives z more accurate flow than with a box filter,
+            at the cost of lower speed; normally, winsize for a Gaussian window should
+            be set to a larger value to achieve the same level of robustness.
+
+    Returns
+    -------
+    Array
+        An array of motion vectors relative to the reference image. If shape of
+        input frames are (N, M, T) then the shape of the output is (N, M, T, 2).
+    """
+
+    logger.info("Get velocities using Farneback's algorithm")
+
+    all_flows = []
+    for index in range(frames.shape[-1] - 1):
+        all_flows.append(
+            dask.delayed(flow)(
+                frames[:, :, index + 1],
+                frames[:, :, index],
+                pyr_scale,
+                levels,
+                winsize,
+                iterations,
+                poly_n,
+                poly_sigma,
+                flags,
+            ),
+        )
+
+    with ProgressBar():
         flows = da.stack(*da.compute(all_flows), axis=2)
 
-    if filter_options:
-        flows = utils.filter_vectors_par(flows, **filter_options)
     logger.info("Done running Farneback's algorithm")
 
     return flows
