Implement stats module and fix some bugs related to velocity computation

mps_motion_tracking/mechanics.py
@@ -98,14 +98,14 @@ def compute_velocity(u: Array, t: Array):
     dt = da.diff(t)
 
     # Need to have time axis
-    return da.moveaxis(da.moveaxis(du, 2, 3) * 1000 / dt, 2, 3)
+    return da.moveaxis(da.moveaxis(du, 2, 3) / dt, 2, 3)
 
 
 def compute_displacement(v: Array, t: Array, ref_index=0):
     """Compute displacement from velocity"""
 
     zero = da.zeros((v.shape[0], v.shape[1], 1, v.shape[3]))
-    dt = np.diff(t) / 1000
+    dt = np.diff(t)
     vdt = da.apply_along_axis(lambda x: x * dt, axis=2, arr=v)
 
     vdt_low = vdt[:, :, :ref_index, :]
@@ -141,8 +141,12 @@ class Mechanics:
             relevant when computing time derivaties such as velocity.
         """
         assert isinstance(u, fs.VectorFrameSequence)
-        self.u = u
+        self._u = u
         self.t = t
+
+    @property
+    def u(self) -> fs.VectorFrameSequence:
+        return self._u
 
     @property
     def t(self) -> Array:

tests/conftest.py
@@ -1,10 +1,13 @@
 from pathlib import Path
+from unittest import mock
 
 import numpy as np
 import pytest
 from scipy.ndimage import geometric_transform
 
-from mps_motion_tracking.utils import MPSData
+from mps_motion_tracking import frame_sequence as fs
+from mps_motion_tracking import Mechanics
+from mps_motion_tracking import MPSData
 
 here = Path(__file__).absolute().parent
 
@@ -46,6 +49,45 @@ def test_data():
     }
 
     return MPSData(frames=frames, time_stamps=times, info=info)
+
+
+@pytest.fixture
+def mech_obj():
+
+    width = 10
+    height = 15
+    num_time_points = 4
+
+    u = np.zeros((height, width, num_time_points, 2))
+    # First time point is zero
+    # Second time point has a linar displacement in x
+    u[:, :, 1, 0] = np.fromfunction(
+        lambda y, x: x / width,
+        shape=(height, width),
+        dtype=float,
+    )
+    # Third points have linear displacement in y
+    u[:, :, 2, 1] = np.fromfunction(
+        lambda y, x: y / height,
+        shape=(height, width),
+        dtype=float,
+    )
+    # Forth is linear in both
+    u[:, :, 2, 0] = u[:, :, 1, 0]
+    u[:, :, 3, 1] = u[:, :, 2, 1]
+
+    return Mechanics(fs.VectorFrameSequence(u), t=1000 * np.arange(4))
+
+
+@pytest.fixture
+def mech_trace_obj():
+    traces = np.load(here.joinpath("example_traces.npy"), allow_pickle=True).item()
+
+    mechanics_mock = mock.Mock(spec=Mechanics)
+    mechanics_mock.u.norm().mean.return_value = traces["u_norm"]
+    mechanics_mock.velocity.norm().mean.return_value = traces["v_norm"]
+    mechanics_mock.t = traces["time"]
+    return mechanics_mock
 
 
 # @pytest.fixture

tests/test_mechanics.py
@@ -125,8 +125,7 @@ def test_dx(dx):
 
 
 def test_velocity(mech_obj):
-    v = mech_obj.velocity
-
+    v = mech_obj.velocity * 1000
     assert np.isclose(
         v[:, :, 0, 0],
         mech_obj.u[:, :, 1, 0] - mech_obj.u[:, :, 0, 0],
@@ -149,34 +148,6 @@ def test_compute_displacement(mech_obj):
     v = mech_obj.velocity
     u = mechanics.compute_displacement(v.array, mech_obj.t, ref_index=0)
     assert da.isclose(u, mech_obj.u.array).all().compute()
-
-
-@pytest.fixture
-def mech_obj():
-
-    width = 10
-    height = 15
-    num_time_points = 4
-
-    u = np.zeros((height, width, num_time_points, 2))
-    # First time point is zero
-    # Second time point has a linar displacement in x
-    u[:, :, 1, 0] = np.fromfunction(
-        lambda y, x: x / width,
-        shape=(height, width),
-        dtype=float,
-    )
-    # Third points have linear displacement in y
-    u[:, :, 2, 1] = np.fromfunction(
-        lambda y, x: y / height,
-        shape=(height, width),
-        dtype=float,
-    )
-    # Forth is linear in both
-    u[:, :, 2, 0] = u[:, :, 1, 0]
-    u[:, :, 3, 1] = u[:, :, 2, 1]
-
-    return Mechanics(fs.VectorFrameSequence(u), t=1000 * np.arange(4))
 
 
 def test_shapes():

tests/test_motion_tracking.py
@@ -1,4 +1,3 @@
-import itertools as it
 from unittest import mock
 
 import dask.array as da
@@ -6,6 +5,7 @@ import numpy as np
 import pytest
 
 from mps_motion_tracking import FLOW_ALGORITHMS as _FLOW_ALGORITHMS
+from mps_motion_tracking import Mechanics
 from mps_motion_tracking import OpticalFlow
 from mps_motion_tracking import utils
 
@@ -127,17 +127,52 @@ def test_get_displacement_unit(test_data: utils.MPSData):
 
 
 @pytest.mark.parametrize(
-    "flow_algorithm, unit",
-    it.product(FLOW_ALGORITHMS, ["um", "pixels"]),
+    "unit",
+    ["um", "pixels"],
 )
-def test_get_displacement_scale_algs(
+def test_get_displacement_scale(
     test_data: utils.MPSData,
-    flow_algorithm: _FLOW_ALGORITHMS,
     unit: str,
 ):
     """Test that there are now exceptions raised"""
-    m = OpticalFlow(test_data, flow_algorithm=flow_algorithm)
-    m.get_displacements(unit=unit, scale=0.5)
+    m = OpticalFlow(test_data)
+    u_full = m.get_displacements(unit=unit)
+    u = m.get_displacements(unit=unit, scale=0.5)
+    assert np.isclose(u.mean().max().compute(), u_full.mean().max().compute())
+
+
+@pytest.mark.parametrize(
+    "unit",
+    ["um", "pixels"],
+)
+def test_get_velocity_scale(
+    test_data: utils.MPSData,
+    unit: str,
+):
+    """Test that there are now exceptions raised"""
+    m = OpticalFlow(test_data)
+    v_full = m.get_velocities(unit=unit)
+    v = m.get_velocities(unit=unit, scale=0.5)
+    u_full = m.get_displacements(unit=unit, scale=0.5)
+    v_full_from_u = Mechanics(u_full, t=test_data.time_stamps).velocity
+
+    u = m.get_displacements(unit=unit)
+    v_from_u = Mechanics(u, t=test_data.time_stamps).velocity
+
+    # We cannot expected these to be all equal, but they should be
+    # of the save order, so a 5-10% difference is OK
+    assert np.isclose(v.mean().max().compute(), v_full.mean().max().compute(), rtol=0.1)
+
+    assert np.isclose(
+        v_from_u.mean().max().compute(),
+        v_full_from_u.mean().max().compute(),
+        rtol=0.1,
+    )
+    assert np.isclose(
+        v_from_u.mean().max().compute(),
+        v.mean().max().compute(),
+        rtol=0.1,
+    )
 
 
 def test_OpticalFlow_options(test_data: utils.MPSData):

tests/test_scaling.py
@@ -18,7 +18,7 @@ def test_resize_data():
     assert np.isclose(scaled_data.time_stamps, times).all()
 
 
-def test_resize_frames():
+def test_resize_frames_displacement():
     width = 10
     height = 15
     num_time_points = 4