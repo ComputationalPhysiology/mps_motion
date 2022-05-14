3d6a46495b8568829f8992bb31e0cedcba9a3d14
Henrik Finsberg <henriknf@simula.no>
2022-05-14 22:36:44 +0200

    Make it possible to control thickness of arrows in quiver plot

--- mps_motion_tracking/visu.py
+++ mps_motion_tracking/visu.py
@@ -53,14 +53,14 @@ def colorize(img, vmin=None, vmax=None, factor=1, cmap="bwr"):
     return img
 
 
-def _draw_flow(image, x, y, fx, fy):
+def _draw_flow(image, x, y, fx, fy, thickness: int = 2):
     QUIVER = (0, 0, 255)
     lines = np.vstack([x, y, x + fx, y + fy]).T.reshape(-1, 2, 2)
     lines = np.int32(lines + 0.5)
     vis = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
-    cv2.polylines(vis, lines, 0, QUIVER, thickness=5, lineType=8)
-    for (x1, y1), (_x2, _y2) in lines:
-        cv2.circle(vis, (x1, y1), 1, (0, 255, 0), -1)
+    cv2.polylines(vis, lines, 0, QUIVER, thickness=thickness, lineType=8)
+    # for (x1, y1), (_x2, _y2) in lines:
+    #     cv2.circle(vis, (x1, y1), 1, (0, 255, 0), -1)
     return vis
 
 
@@ -70,7 +70,7 @@ def draw_lk_flow(image, flow, reference_points):
     return _draw_flow(image, x, y, fx, fy)
 
 
-def draw_flow(image, flow, step=16, scale: float = 1.0):
+def draw_flow(image, flow, step=16, scale: float = 1.0, thickness: int = 2):
     """[summary]
 
     Parameters
@@ -90,7 +90,7 @@ def draw_flow(image, flow, step=16, scale: float = 1.0):
     h, w = image.shape[:2]
     y, x = np.mgrid[step / 2 : h : step, step / 2 : w : step].reshape(2, -1).astype(int)
     fx, fy = flow[y, x].T
-    return _draw_flow(image, x, y, scale * fx, scale * fy)
+    return _draw_flow(image, x, y, scale * fx, scale * fy, thickness=thickness)
 
 
 def draw_hsv(flow):
@@ -123,6 +123,7 @@ def quiver_video(
     frame_scale: float = 1.0,
     convert: bool = True,
     offset: int = 0,
+    thickness: int = 2,
 ) -> None:
 
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
@@ -178,7 +179,9 @@ def quiver_video(
     for i in tqdm.tqdm(range(num_frames), desc=f"Create quiver video at {p}"):
         im = frames[:, :, i]
         flow = np.take(vectors_np, i, axis=time_axis)
-        out.write(draw_flow(im, flow, step=step, scale=vector_scale))
+        out.write(
+            draw_flow(im, flow, step=step, scale=vector_scale, thickness=thickness),
+        )
 
     out.release()
     cv2.destroyAllWindows()
